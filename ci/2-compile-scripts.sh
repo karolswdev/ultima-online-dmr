b7a2ff4ae6e8e6a0871759d5f2f224372abaf6b7
Karol <karolsane@gmail.com>
2020-03-30 11:59:27 -0600

    Debugging added to CI pipeline

--- ci/2-compile-scripts.sh
+++ ci/2-compile-scripts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+echo $pwd
+echo "Running docker run -v $pwd/src/pkg:/pkg -v $pwd/src/scripts:/scripts ultimadmr/dmr-ecompiler"
 docker run -v $pwd/src/pkg:/pkg -v $pwd/src/scripts:/scripts ultimadmr/dmr-ecompiler
